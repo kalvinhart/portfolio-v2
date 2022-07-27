555826d7430f1a123256ce247282fc7587a84a5b
Kalvin <kalvinhart@hotmail.co.uk>
2022-07-27 22:21:18 +0100

    style: added heading styles

---
+++ client/src/styles/fonts.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+  font-size: 76px;
+`;
+
+export const H2 = styled.h2`
+  font-size: 48px;
+`;
+
+export const SpanHeroTitle = styled.span`
+  font-size: 54px;
+`;
