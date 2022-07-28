581f67fded83633f4d041b7ab0ca5079af0b5e50
Kalvin <kalvinhart@hotmail.co.uk>
2022-07-28 11:39:39 +0100

    added about section content

--- client/src/components/About/About.styles.ts
+++ client/src/components/About/About.styles.ts
@@ -1,11 +1,52 @@
 import styled from "styled-components";
+import { Paragraph } from "../../styles/fonts";
 import { slopedSectionBottom, slopedSectionTop } from "../../styles/mixins";
 
 export const AboutWrapper = styled.section`
   position: relative;
-  padding: 40px 0;
+  padding: 60px 0 80px;
   color: #111;
 
   ${slopedSectionTop}
   ${slopedSectionBottom}
+`;
+
+export const AboutContentWrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const AboutContentText = styled.div`
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const AboutContentSkills = styled.div`
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+
+  & ${Paragraph} {
+    margin-bottom: 40px;
+  }
+`;
+
+export const AboutSkillsImg = styled.img`
+  width: 35px;
+  margin-right: 20px;
+`;
+
+export const AboutSkillsUL = styled.ul`
+  list-style: none;
+`;
+
+export const AboutSkillsLI = styled.li`
+  display: flex;
+  align-items: center;
+
+  &:not(:last-child) {
+    margin-bottom: 10px;
+  }
 `;
