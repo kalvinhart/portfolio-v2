b8a1613f277a00f29cccd140344cd38863c591ca
Kalvin <kalvinhart@hotmail.co.uk>
2022-07-28 21:28:43 +0100

    added primary colour span

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -1,5 +1,5 @@
 import { MainContainer } from "../../styles/containers";
-import { H1, SpanHeroTitle, SpanLarge } from "../../styles/fonts";
+import { H1, SpanHeroTitle, SpanLarge, SpanPrimary } from "../../styles/fonts";
 import { HeroContentWrapper, HeroWrapper } from "./Hero.styles";
 
 const Hero = () => {
@@ -7,7 +7,9 @@ const Hero = () => {
     <HeroWrapper id="hero">
       <MainContainer>
         <HeroContentWrapper>
-          <H1>Kalvin Hart</H1>
+          <H1>
+            Kalvin <SpanPrimary>Hart</SpanPrimary>
+          </H1>
           <SpanHeroTitle>Full stack developer</SpanHeroTitle>
           <SpanLarge>React / TypeScript / Node / Express / MongoDB</SpanLarge>
         </HeroContentWrapper>
